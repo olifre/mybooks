24d8a1fdcaa02e74b688d5f4da0eb0e6e310891b
Oliver Freyermuth <o.freyermuth@googlemail.com>
2021-07-17 03:05:33 +0200

    Add book "Second in Line" by OF.

---
+++ Oliver Freyermuth/01 - Second in Line/chapters/.dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((latex-mode . ((TeX-master . "../Oliver_Freyermuth_-_Second_in_Line.tex")
+								(ispell-local-dictionary . "german")
+								(LaTeX-command . "latex -shell-escape -synctex=1 --file-line-error-style"))))
