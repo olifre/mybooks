b5a99982bdcd042d1ade86b8974b9a40ffc52dc3
Oliver Freyermuth <o.freyermuth@googlemail.com>
2022-05-07 19:50:01 +0200

    gh-action: Slightly improve sitemap formatting.

--- tools/gen_sitemap.sh
+++ tools/gen_sitemap.sh
@@ -10,10 +10,10 @@ EOD
 NOW_DATE=$(date +%Y-%m-%d)
 
 while IFS= read -r -d '' CUR_FILE; do
-	echo "<url>" >> sitemap.xml
+	echo "  <url>" >> sitemap.xml
 	# Canonicalize URLs.
 	CUR_FILE=$(echo ${CUR_FILE} | sed 's:index.html$::')
-	echo "  <loc>${BASE}/${CUR_FILE}</loc>" >> sitemap.xml
+	echo "    <loc>${BASE}/${CUR_FILE}</loc>" >> sitemap.xml
 	LAST_MODIFIED=${NOW_DATE}
 	if [[ ${CUR_FILE} == *.html ]]; then
 		# It's an HTML file, try to deduce last modified time.
@@ -21,8 +21,8 @@ while IFS= read -r -d '' CUR_FILE; do
 			LAST_MODIFIED=$(cat "${CUR_FILE}" | grep "http-equiv=.last-modified" | \
 					grep -Po "<meta.*content='\K[[:digit:]]+-[[:digit:]]+-[[:digit:]]+")
 	fi
-	echo "  <lastmod>${LAST_MODIFIED}</lastmod>" >> sitemap.xml
-	echo "</url>" >> sitemap.xml
+	echo "    <lastmod>${LAST_MODIFIED}</lastmod>" >> sitemap.xml
+	echo "  </url>" >> sitemap.xml
 done < <(find . -type f \( -iname "*.html" -o -iname "*.pdf" \) -printf '%P\0')
 
 echo "</urlset>" >> sitemap.xml
