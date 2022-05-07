b3129aed3c2084a4f7ce845ee343eefac365ab41
Oliver Freyermuth <o.freyermuth@googlemail.com>
2022-05-07 19:41:38 +0200

    gh-action: In gen_sitemap, extract last-modified from HTML.
    If no last-modified is found, assume "now" for all files.

--- tools/gen_sitemap.sh
+++ tools/gen_sitemap.sh
@@ -7,11 +7,21 @@ cat > sitemap.xml <<EOD
 <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 EOD
 
-while IFS= read -r -d '' HTMLFILE; do
+NOW_DATE=$(date +%Y-%m-%d)
+
+while IFS= read -r -d '' CUR_FILE; do
 	echo "<url>" >> sitemap.xml
 	# Canonicalize URLs.
-	HTMLFILE=$(echo ${HTMLFILE} | sed 's:index.html$::')
-	echo "<loc>${BASE}/${HTMLFILE}</loc>" >> sitemap.xml
+	CUR_FILE=$(echo ${CUR_FILE} | sed 's:index.html$::')
+	echo "  <loc>${BASE}/${CUR_FILE}</loc>" >> sitemap.xml
+	LAST_MODIFIED=${NOW_DATE}
+	if [[ ${CUR_FILE} == *.html ]]; then
+		# It's an HTML file, try to deduce last modified time.
+		grep -q "http-equiv=.last-modified" "${CUR_FILE}" && \
+			LAST_MODIFIED=$(cat "${CUR_FILE}" | grep "http-equiv=.last-modified" | \
+					grep -Po "<meta.*content='\K[[:digit:]]+-[[:digit:]]+-[[:digit:]]+")
+	fi
+	echo "  <lastmod>${LAST_MODIFIED}</lastmod>" >> sitemap.xml
 	echo "</url>" >> sitemap.xml
 done < <(find . -type f \( -iname "*.html" -o -iname "*.pdf" \) -printf '%P\0')
 
