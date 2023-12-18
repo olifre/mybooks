00734aa79638e4a6add3fc9b2698ae79691e7c4a
Oliver Freyermuth <o.freyermuth@googlemail.com>
2023-12-19 00:13:34 +0100

    HTML: add (optional) javascript to show read progress bar.
    Shown at the top of every page with book content.

---
+++ shared/js/readprogress.js
@@ -0,0 +1,18 @@
+// Create HTML elements to show read progress bar.
+const progress_container = document.createElement("div");
+progress_container.className = "progress-container";
+const progress_bar = document.createElement("div");
+progress_bar.className = "progress-bar";
+progress_bar.id = "read-bar";
+progress_container.appendChild(progress_bar);
+document.body.prepend(progress_container);
+
+// When the user scrolls the page, update read progress.
+window.onscroll = function() {updateReadProgress()};
+
+function updateReadProgress() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("read-bar").style.width = scrolled + "%";
+}
